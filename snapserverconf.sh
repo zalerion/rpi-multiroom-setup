d99d936af5e4c6445d4bb222db38386b5545d4ff
zalerion <65732025+zalerion@users.noreply.github.com>
2020-05-23 20:37:50 +0200

    Add files via upload

--- snapserverconf.sh
+++ snapserverconf.sh
@@ -1,1 +1,3 @@
-if [ "$1" = "yes" ]; then cd ./res/snapserver cat 1 2_streams 3 > /etc/snapserver.conf cd .. fi
+if [ "$1" = "yes" ]; then 
+cd ./res/snapserver cat 1 2_streams 3 > /etc/snapserver.conf cd .. 
+fi
