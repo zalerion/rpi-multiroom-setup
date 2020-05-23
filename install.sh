e628ba24078038a33f7464ffec8b26153ff41370
zalerion <65732025+zalerion@users.noreply.github.com>
2020-05-23 20:23:12 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
-#!/bin/bash 
-
-if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-
+#!/bin/bash
+if [[ $(id -u) -ne 0 ]]; then echo "Please run as root" exit 1; fi
 sudo rm /etc/asound.conf
-#starting with clean asound.conf
+##starting with clean asound.conf
+
 
 server='no'
-mpd=1
+mpd="1"
 read -p "Do you want to setup the Pi as Snapserver [y/N] " REPLY
-if [[ "$REPLY" =~ ^(yes|y|Y)$ ]]; 
-then 
+#if [[ "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
+if [[ "$REPLY" =~ ^(yes|y|Y)$ ]]; then
 	server='yes'; 
 	read -p "Do you want to stream from multiple MPD servers?  [y/N] " REPLY
 	if [[ "$REPLY" =~ ^(yes|y|Y)$ ]]; then 
@@ -31,22 +30,23 @@ then
 
 fi
 
-#echo " $((42-mpd))"
+##echo " $((42-mpd))"
 
-sudo chmod 755 snapclient.sh
-sudo chmod 755 snapserver.sh
-sudo chmod 755 snapfinish.sh
-sudo chmod 755 mpd.sh
-sudo chmod 755 mountnas.sh
-sudo chmod 755 setTimeSync.sh
-sudo chmod 755autostartSetup.sh
+#sudo chmod 755 snapclient.sh
+#sudo chmod 755 snapserver.sh
+#sudo chmod 755 snapfinish.sh
+#sudo chmod 755 mpd.sh
+#sudo chmod 755 mountnas.sh
+#sudo chmod 755 setTimeSync.sh
+#sudo chmod 755autostartSetup.sh
 
-sudo ./autostartSetup.sh
-sudo ./setTimeSync.sh
-sudo ./snapserver.sh $server $mpd
-sudo ./mpd.sh $server $mpd #also calls the mpdSetup
-sudo ./snapclient.sh
-sudo ./snapfinish.sh
-sudo ./btlInstall.sh $server
-sudo ./snapserverconf.sh $server
-sudo ./mountnas.sh
+#sudo ./autostartSetup.sh
+#sudo ./setTimeSync.sh
+#sudo ./snapserver.sh $server $mpd
+#sudo ./mpd.sh $server $mpd #also calls the mpdSetup
+#sudo ./snapclient.sh
+#sudo ./snapfinish.sh
+#sudo ./btlInstall.sh $server
+#sudo ./snapserverconf.sh $server
+#sudo ./mountnas.sh
+
