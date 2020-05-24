c7b9d12cba424f06253f300808ae75313b9bd2a5
zalerion <65732025+zalerion@users.noreply.github.com>
2020-05-24 12:57:29 +0200

    fixed #!/bash/bin

--- install.sh
+++ install.sh
@@ -34,13 +34,17 @@ fi
 cd scripts
 ##echo " $((42-mpd))"
 
-sudo chmod 755 snapclient.sh
-sudo chmod 755 snapserver.sh
-sudo chmod 755 snapfinish.sh
-sudo chmod 755 mpd.sh
-sudo chmod 755 mountnas.sh
-sudo chmod 755 setTimeSync.sh
 sudo chmod 755 autostartSetup.sh
+sudo chmod 755 setTimeSync.sh
+sudo chmod 755 snapserver.sh
+sudo chmod 755 mpd.sh
+sudo chmod 755 mpdSetup.sh
+sudo chmod 755 snapclient.sh
+sudo chmod 755 snapfinish.sh
+sudo chmod 755 btlInstall.sh
+sudo chmod 755 btlSetup.sh
+sudo chmod 755 snapserverconf.sh
+sudo chmod 755 mountnas.sh
 
 sudo ./autostartSetup.sh
 sudo ./setTimeSync.sh
