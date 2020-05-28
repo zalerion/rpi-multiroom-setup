fd49e7112986249612f2ffed8f2cc0c72d2d9bc6
zalerion <65732025+zalerion@users.noreply.github.com>
2020-05-28 19:02:43 +0200

    .

--- scripts/btlInstall.sh
+++ scripts/btlInstall.sh
@@ -2,23 +2,21 @@
 
 server="$1"     # yes / no
 
-
-read -p "Do you want to setup the bluetooth receiver? This will use the rpi-audio-receiver script by nico kaiser [y/N] " REPLY
+echo
+echo "Thanks to nico kaiser for providing his rpi-audio-receiver scripts!"
+read -p "Do you want to setup the bluetooth receiver? [y/N] " REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
 
-wget -q https://github.com/nicokaiser/rpi-audio-receiver/archive/master.zip
-unzip master.zip
-rm master.zip
+#wget -q https://github.com/nicokaiser/rpi-audio-receiver/archive/master.zip
+#unzip master.zip
+#rm master.zip
 
 cd rpi-audio-receiver-master
 sudo ./install-bluetooth.sh
 
-echo "1"
 cd ..
-echo "2"
 rm -r rpi-audio-receiver-master
-echo "3"
 if [ $server = "yes" ]; then
 sudo ./btlSetup.sh
 fi
