cfab3dcd6bb38440743d06de4a5a5ee8d97fe631
zalerion <65732025+zalerion@users.noreply.github.com>
2020-05-30 23:38:13 +0200

    Update btlSetup.sh

--- scripts/btlSetup.sh
+++ scripts/btlSetup.sh
@@ -7,9 +7,6 @@ echo "BTL SETUP"
 echo "stream = pipe:///tmp/bluesnapfifo?name=BluetoothStream&sampleformat=44100:16:2" >> ./res/snapserver/2_streams
 #rm tmp
 
-sudo apt-get update
-sudo apt-get upgrade --yes
-
 cat <<EOM >> /etc/asound.conf
 ###########################################################################
 #
