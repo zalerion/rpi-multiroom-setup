e22a5c9392983b5dd51d7cc1f6c8c67e2077471c
zalerion <65732025+zalerion@users.noreply.github.com>
2020-05-31 13:33:02 +0200

    Update btlInstall.sh

--- scripts/btlInstall.sh
+++ scripts/btlInstall.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-#server="$1"     # yes / no
+#btl="$1"     # yes / no
 
 echo
-#echo "Thanks to nico kaiser for providing his rpi-audio-receiver scripts!"
-#read -p "Do you want to setup the bluetooth receiver? [y/N] " REPLY
-
-if [[ ! $1 =~ (yes|y|Y) ]]; then exit 0; fi
-
-
-
-#wget -q https://github.com/nicokaiser/rpi-audio-receiver/archive/master.zip
-#unzip master.zip
-#rm master.zip
+if [[ ! "$1" =~ (yes|y|Y) ]]; then exit 0; fi
 
 cd rpi-audio-receiver-master
 sudo ./install-bluetooth.sh
-
 cd ..
-#rm -r rpi-audio-receiver-master
-
-#if [ $server = "yes" ]; then
-#sudo ./btlSetup.sh
-#fi
-
-
