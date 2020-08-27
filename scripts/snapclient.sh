778413bd663430b5166a48fa05db4ed3f55be523
zalerion <65732025+zalerion@users.noreply.github.com>
2020-08-27 22:27:38 +0200

    Update snapclient.sh

--- scripts/snapclient.sh
+++ scripts/snapclient.sh
@@ -7,8 +7,8 @@ if [[ ! "$1" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
 
 
-wget https://github.com/badaix/snapcast/releases/download/v0.19.0/snapclient_0.19.0-1_armhf.deb
-sudo dpkg -i snapclient_0.19.0-1_armhf.deb
+wget https://github.com/badaix/snapcast/releases/download/v0.20.0/snapclient_0.20.0-1_armhf.deb
+sudo dpkg -i snapclient_0.20.0-1_armhf.deb
 
 
 
@@ -16,7 +16,7 @@ echo "Updating dependencies and removing downloaded files"
 sudo apt-get -f install --yes
 sudo apt-get update
 sudo apt-get upgrade --yes
-rm snapclient_0.19.0-1_armhf.deb
+rm snapclient_0.20.0-1_armhf.deb
 
 
 #setup for server only
