c7609831503ded9ceb18f769c3af78e0ab846104
zalerion <65732025+zalerion@users.noreply.github.com>
2020-12-29 18:55:39 +0100

    Update snapclient.sh

--- scripts/snapclient.sh
+++ scripts/snapclient.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #echo
-#echo -n "Do you want to install Snapclient v0.19.0? [y/N] "
+#echo -n "Do you want to install Snapclient v0.22.0? [y/N] "
 #read REPLY
 if [[ ! "$1" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
 
 
-wget https://github.com/badaix/snapcast/releases/download/v0.20.0/snapclient_0.20.0-1_armhf.deb
-sudo dpkg -i snapclient_0.20.0-1_armhf.deb
+wget https://github.com/badaix/snapcast/releases/download/v0.22.0/snapclient_0.22.0-1_armhf.deb
+sudo dpkg -i snapclient_0.22.0-1_armhf.deb
 
 
 
@@ -16,7 +16,7 @@ echo "Updating dependencies and removing downloaded files"
 sudo apt-get -f install --yes
 sudo apt-get update
 sudo apt-get upgrade --yes
-rm snapclient_0.20.0-1_armhf.deb
+rm snapclient_0.22.0-1_armhf.deb
 
 
 #setup for server only
