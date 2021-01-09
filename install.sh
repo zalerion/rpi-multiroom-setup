f2f6fe10f26370572f6a483df8902dd2e7fbe19f
zalerion <65732025+zalerion@users.noreply.github.com>
2021-01-09 16:23:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,55 +27,55 @@ echo "[2] Audio server (select for local server/receiver or multiroom server set
 echo
 read -p "Please choose this systems general purpose [1/2]" REP
 
-if [ $REP = "1" ];then
-	echo
-	sclient="yes"
-	oclient="yes"
-elif [ $REP = "2" ];then
-	echo
-
-	read -p "Would you like to set up this pi as snapserver? [y/N] " REP
-	if [[ $REP =~ ^(yes|y|Y)$ ]]; then server="yes"; 
-		################
-		#spotify "without snapserver" not configured yet.
-		################
-		echo
-		echo "Thanks to dtcooper for his easy raspotify setup!"
-		read -p "Do you want to setup as spotify connect speaker? [y/N] " REP
-		if [[ $REP =~ ^(yes|y|Y)$ ]]; then spotify="yes"; fi
-
-	fi
-	echo
-	read -p "Would you like to set up MPD? [y/N] " REP
-	if [[ $REP =~ ^(yes|y|Y)$ ]]; then 
-		mpd="yes";
-		if [ $server = "yes" ];then
-			read -p "How many MPD instances? [1-80] " REP
-			mpd=$REP
-		fi		
-	fi
-	
-	
-	echo
-	echo "Thanks to nico kaiser for providing his rpi-audio-receiver scripts!"
-	read -p "Do you want to setup the bluetooth receiver? [y/N] " REP
-	if [[ $REP =~ ^(yes|y|Y)$ ]]; then btl="yes"; fi
-
-
-
-	read -p "Would you like to also use this pi as snapclient? [y/N] " REP
-	if [[ $REP =~ ^(yes|y|Y)$ ]]; then sclient="yes"; fi
-
-fi
-
-	read -p "Would you like to set up a dsp? [y/N] " REP
-	if [[ $REP =~ ^(yes|y|Y)$ ]]; then dsp="yes"; fi
-	
-	read -p "Would you like to set use an Audio-Hat? [y/N] " REP
-	if [[ $REP =~ ^(yes|y|Y)$ ]]; then hat="yes"; fi
-
-
-
+#if [ $REP = "1" ];then
+#	echo
+#	sclient="yes"
+#	oclient="yes"
+#elif [ $REP = "2" ];then
+#	echo
+#
+#	read -p "Would you like to set up this pi as snapserver? [y/N] " REP
+#	if [[ $REP =~ ^(yes|y|Y)$ ]]; then server="yes"; 
+#		################
+#		#spotify "without snapserver" not configured yet.
+#		################
+#		echo
+#		echo "Thanks to dtcooper for his easy raspotify setup!"
+#		read -p "Do you want to setup as spotify connect speaker? [y/N] " REP
+#		if [[ $REP =~ ^(yes|y|Y)$ ]]; then spotify="yes"; fi
+#
+#	fi
+#	echo
+#	read -p "Would you like to set up MPD? [y/N] " REP
+#	if [[ $REP =~ ^(yes|y|Y)$ ]]; then 
+#		mpd="yes";
+#		if [ $server = "yes" ];then
+#			read -p "How many MPD instances? [1-80] " REP
+#			mpd=$REP
+#		fi		
+#	fi
+#	
+#	
+#	echo
+#	echo "Thanks to nico kaiser for providing his rpi-audio-receiver scripts!"
+#	read -p "Do you want to setup the bluetooth receiver? [y/N] " REP
+#	if [[ $REP =~ ^(yes|y|Y)$ ]]; then btl="yes"; fi
+#
+#
+#
+#	read -p "Would you like to also use this pi as snapclient? [y/N] " REP
+#	if [[ $REP =~ ^(yes|y|Y)$ ]]; then sclient="yes"; fi
+#
+#fi
+#
+#	read -p "Would you like to set up a dsp? [y/N] " REP
+#	if [[ $REP =~ ^(yes|y|Y)$ ]]; then dsp="yes"; fi
+#	
+#	read -p "Would you like to set use an Audio-Hat? [y/N] " REP
+#	if [[ $REP =~ ^(yes|y|Y)$ ]]; then hat="yes"; fi
+#
+#
+#
 
 
 #read -p "debug"
