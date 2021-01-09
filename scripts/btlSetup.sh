5039c8f8c9c6f4722f7a72cb887bef6f2848a664
zalerion <65732025+zalerion@users.noreply.github.com>
2021-01-09 16:47:07 +0100

    Update btlSetup.sh

--- scripts/btlSetup.sh
+++ scripts/btlSetup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+source options.conf
 
-if [[ ! "$2" =~ (yes|y|Y) ]]; then exit 0; fi
+if [[ ! $btl =~ (yes|y|Y) ]]; then exit 0; fi
 echo "sudo hciconfig hci0 reset" >> /etc/autostart.sh 
 
-if [[ ! "$1" =~ (yes|y|Y) ]]; then exit 0; fi
+if [[ ! $server =~ (yes|y|Y) ]]; then exit 0; fi
 
 echo "BTL SETUP"
 echo "stream = pipe:///tmp/bluesnapfifo?name=BluetoothStream&sampleformat=44100:16:2" >> ./res/snapserver/2_streams
