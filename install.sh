b2721591d1eee9a085636a46fe346386e8140292
zalerion <65732025+zalerion@users.noreply.github.com>
2021-01-09 16:21:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -93,6 +93,7 @@ fi
 ##################
 
 sudo chmod 755 setRights.sh
+sudo ./setRights.sh
 
 #cd scripts
 
@@ -121,29 +122,8 @@ sudo chmod 755 setRights.sh
 ##################
 #calling actual install and config scripts
 ##################
-sudo ./setupAudioHat.sh $hat
 
-sudo ./autostartSetup.sh
-sudo ./mountnas.sh $oclient $mpd
-sudo ./mpdSetup.sh $server $mpd
-
-sudo ./dspSetup.sh $dsp
-
-sudo ./spotifyConnectInstall.sh $server $spotify
-sudo ./snapserver.sh $server $mpd
-sudo ./snapclient.sh $sclient $oclient
-
-#read -p "btl=$btl, server=$server"
-sudo ./btlInstall.sh $btl
-sudo ./btlSetup.sh $server $btl
-
-#sudo apt update
-#sudo apt upgrade --yes
-
-sudo ./mpd.sh $server $mpd 
-sudo ./snapserverconf.sh $server
-sudo ./setTimeSync.sh
-sudo ./asoundConf.sh $sclient $btl $dsp
+sudo callSkripts.sh
 
 echo
 echo
