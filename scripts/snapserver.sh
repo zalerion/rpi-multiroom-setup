d799dcb2ab96aa756f1a2d4c3e07fb15b98014b5
zalerion <65732025+zalerion@users.noreply.github.com>
2021-01-09 17:31:16 +0100

    Update snapserver.sh

--- scripts/snapserver.sh
+++ scripts/snapserver.sh
@@ -5,7 +5,7 @@ if [ $server == "no" ]
   then 
     exit 0;
 fi
-wget https://github.com/badaix/snapcast/releases/download/v0.20.0/snapserver_0.22.0-1_armhf.deb
+wget https://github.com/badaix/snapcast/releases/download/v0.22.0/snapserver_0.22.0-1_armhf.deb
 sudo dpkg -i snapserver_0.22.0-1_armhf.deb
 
 echo "Updating dependencies and removing downloaded files"
