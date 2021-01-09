b663993db176cca528ff988330c0128dcb4ea5b4
zalerion <65732025+zalerion@users.noreply.github.com>
2021-01-09 18:17:33 +0100

    Update snapclient.sh

--- scripts/snapclient.sh
+++ scripts/snapclient.sh
@@ -21,7 +21,7 @@ rm snapclient_0.22.0-1_armhf.deb
 
 
 #setup for server only
-if [[ "$2" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
+if [[ $server =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 sudo systemctl disable snapclient.service
 echo "snapclient -s snapclient &" >> /etc/autostart.sh
 #cat <<EOM >> /etc/asound.conf
