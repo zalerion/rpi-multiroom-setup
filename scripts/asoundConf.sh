62e30c7c584512f460a7775670b148ac755b462c
zalerion <65732025+zalerion@users.noreply.github.com>
2021-05-18 12:10:48 +0200

    Update asoundConf.sh

--- scripts/asoundConf.sh
+++ scripts/asoundConf.sh
@@ -5,20 +5,30 @@ source options.conf
 #snapclient="$1" 	# yes / no
 #bluetooth="$2"	# yes / no
 #dsp="$3"        # yes / no
-
+#$server        #yes / no snapserver
 
 
 cd res/asound
 
-
-  if [ $dsp = "yes" ];then
-    cat dsp >> /etc/asound.conf
-  elif [ $sclient = "yes" ];then
-    cat snapclient >> /etc/asound.conf    
-  fi
-  
   if [ $server = "yes" ];then
     if [ $btl = "yes" ];then
       cat bluetooth >> /etc/asound.conf
     fi
+    if [ $sclient = "yes" ];then
+      if [ $dsp = "yes" ];then
+        cat dspSnapserver >> /etc/asound.conf
+      else
+        cat snapclient >> /etc/asound.conf  
+      fi
+    fi
+  else
+    if [ $dsp = "yes" ];then
+      cat dsp >> /etc/asound.conf
+    fi
   fi
+
+
+  if [ $dsp = "yes" ];then
+    cat dsp >> /etc/asound.conf
+  fi
+  
