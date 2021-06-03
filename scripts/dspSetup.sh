1e09e8d3f91dace4a9ce51dcc8cd2cc7dd650b4a
zalerion <65732025+zalerion@users.noreply.github.com>
2021-06-03 14:19:32 +0200

    Update dspSetup.sh

--- scripts/dspSetup.sh
+++ scripts/dspSetup.sh
@@ -14,15 +14,18 @@ sudo wget https://github.com/bmc0/dsp/archive/master.zip
 unzip master
 cd dsp-master
 
-################git clone https://github.com/bmc0/dsp.git
-################cd dsp
-
 make
 sudo make install
 
 cd ..
 rm -rf dsp-master
 rm -rf master.zip
+
+
+
+
+################git clone https://github.com/bmc0/dsp.git
+################cd dsp
 
 sudo mkdir /etc/ladspa_dsp
 #sudo nano /etc/ladspa_dsp/config
@@ -37,22 +40,31 @@ effects_chain= @./eq.txt
 EOM
 
 cat  <<EOM > /etc/ladspa_dsp/eq.txt
-remix 0 0	1 1	
-## 0 is left, 1 is right
-#:0,2 lowpass 1000 0.7	
-## "left" output
-#:1,3 highpass 1000 0.7	
-## "right" output
-remix 0,1 2,3 
-##mixes both channels back
+
+# This demonstrates a split of left and right in high and low channels, compresses the bass, and recombines them. Adapt for own use. (very basic setuop for multiband compression or adapted simulation of dynamic eqs
+
+#remix 0 0 1 1
+#:0,2 lowpass_1 100
+#:1,3 highpass_1 120 gain -6
+#:0,2 ladspa_host sc4_1882.so sc4 - 1.5 - -20 3 - -  ######### compression
+#remix 0,1 2,3
+
+###############################
+#reverse polarity:
+#mult -1
+# used e.g for recombining changed with original signal to hear only the difference
+
+##########
+#more extensive examples for inclúding aditional ladspa plugins
+#ladspa_host  /usr/lib/ladspa/vlevel-ladspa.so vlevel_stereo 2 0.8 - 15 -
+#ladspa_host filter lpf 1000
+#:0,2 ladspa_host lsp-plugins-ladspa http://lsp-plug.in/plugins/ladspa/mb_compressor_stereo 0 1 ####missing lots of parameters
 
 
-# This demonstrates a split of left and right in high and low channels, filters them, and recombines them. Adapt for own use.
-# 
 # eq [Hz] [Q] [Gain] # parametric EQ
 # 
 #remix puts out:
 #remix 0 1 #passthrough
-#remix 0,1 0,1 #mixes both channels back
+#remix 0,1 0,1 #mixes both channels back, stereo to mono
 #remix 1 0 #swaps channels
 EOM
