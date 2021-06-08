35196a3c4cb5a0355fc87a5842d6ef8a0507ae91
zalerion <65732025+zalerion@users.noreply.github.com>
2021-06-08 11:57:18 +0200

    Update snapclient.sh

--- scripts/snapclient.sh
+++ scripts/snapclient.sh
@@ -26,18 +26,17 @@ rm snapclient_0.25.0-1_armhf.deb
 
 #setup for server only
 if [[ ! $server =~ ^(yes|y|Y)$ ]]; then exit 0; fi
-sudo systemctl disable snapclient.service
-echo "snapclient -s snapclient &" >> /etc/autostart.sh
-#cat <<EOM >> /etc/asound.conf
-################################################################################
-##
-##snapclient
-#        #
-#        pcm.snapclient
-#        {
-#                type plug;
-#                slave.pcm "sysdefault:0";
-#        }
-################################################################################
-#EOM
+#sudo systemctl disable snapclient.service
+#echo "snapclient -s snapclient &" >> /etc/autostart.sh
 
+cat <<EOM > /etc/default/snapclient
+
+# Start the client, used only by the init.d script
+START_SNAPCLIENT=true
+
+# Additional command line options that will be passed to snapclient
+# note that user/group should be configured in the init.d script or the systemd unit file
+# For a list of available options, invoke "snapclient --help"
+SNAPCLIENT_OPTS="-s snapclient"
+
+EOM
